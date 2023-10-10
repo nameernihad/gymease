eecfd7cf8600e400a937dc1de1826d7c49df350b
Nameer Nihad PC <nameernihadpc@gmail.com>
2023-10-10 21:47:03 +0530

    api change 2

--- src/Constants/Api.js
+++ src/Constants/Api.js
@@ -1,4 +1,4 @@
 export const userAPI = "https://nn-kicks.online/";
-export const adminAPI = "https://nn-kicks.online/admin";
+export const adminAPI = "https://nn-kicks.online/admin/";
 export const trainerAPI = "https://nn-kicks.online/trainer/";
 export const videoAPI = "https://api.100ms.live/v2/";
