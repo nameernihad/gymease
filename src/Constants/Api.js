76b406c34be1ac7e8111c3135ebd0742fd0a0618
Nameer Nihad PC <nameernihadpc@gmail.com>
2023-10-13 17:04:42 +0530

    api change

--- src/Constants/Api.js
+++ src/Constants/Api.js
@@ -1,9 +1,9 @@
-// export const userAPI = "https://nn-kicks.online/";
-export const userAPI = "http://localhost:4000/";
-// export const adminAPI = "https://nn-kicks.online/admin/";
-export const adminAPI = "http://localhost:4000/admin/";
-// export const trainerAPI = "https://nn-kicks.online/trainer/";
-export const trainerAPI = "http://localhost:4000/trainer/";
+export const userAPI = "https://nn-kicks.online/";
+// export const userAPI = "http://localhost:4000/";
+export const adminAPI = "https://nn-kicks.online/admin/";
+// export const adminAPI = "http://localhost:4000/admin/";
+export const trainerAPI = "https://nn-kicks.online/trainer/";
+// export const trainerAPI = "http://localhost:4000/trainer/";
 
 
 export const videoAPI = "https://api.100ms.live/v2/";
