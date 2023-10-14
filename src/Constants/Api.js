4f6c40c2143809e532c325f39dbc4fdb6f553d84
Nameer Nihad PC <nameernihadpc@gmail.com>
2023-10-14 16:36:21 +0530

    cleanup

--- src/Constants/Api.js
+++ src/Constants/Api.js
@@ -1,8 +1,8 @@
 export const userAPI = "https://nn-kicks.online/";
 // export const userAPI = "http://localhost:4000/";
-export const adminAPI = "https://nn-kicks.online/admin";
+export const adminAPI = "https://nn-kicks.online/admin/";
 // export const adminAPI = "http://localhost:4000/admin/";
-export const trainerAPI = "https://nn-kicks.online/trainer";
+export const trainerAPI = "https://nn-kicks.online/trainer/";
 // export const trainerAPI = "http://localhost:4000/trainer/";
 
 
